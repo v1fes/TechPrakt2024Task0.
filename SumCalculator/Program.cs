15480edc63753c0f139f13cb40c750a609985c36
Fesenko Viktoria <vt221_fvv@student.ztu.edu.ua>
2024-02-17 00:11:02 +0000

    Створено файл .gitignore & README, додано консольний додаток

---
+++ SumCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SumCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            string input1 = Console.ReadLine(); 
+            int number1 = Convert.ToInt32(input1); 
+
+            Console.WriteLine("Enter the second number:");
+            string input2 = Console.ReadLine();
+            int number2 = Convert.ToInt32(input2); 
+
+            int sum = number1 + number2; 
+
+            Console.WriteLine($"{number1} + {number2} = {sum}");
+        }
+    }
+}
